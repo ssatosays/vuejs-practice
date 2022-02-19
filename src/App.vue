f8516c650d3c002b50ff02d4b9733645ab5de8b9
ssatosays <ssatosays@gmail.com>
2022-02-19 23:33:35 +0900

    Create CardArticle.vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <CardArticle title="First Article"/>
+  <CardArticle title="Second Article"/>
+  <CardArticle title="Third Article"/>
+  <CardArticle title="Fourth Article"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import CardArticle from './components/CardArticle.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+    CardArticle
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/CardArticle.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container my-5 p-4 card">
+    <h2>{{ title }}</h2>
+    <div class="row">
+      <div class="col">
+        <p>Primary</p>
+        <button class="btn btn-primary btn-sm">Primary</button>    
+      </div>
+      <div class="col">
+        <p>Secondary</p>
+        <button class="btn btn-secondary btn-sm">Secondary</button>
+      </div>
+      <div class="col">
+        <p>Success</p>
+        <button class="btn btn-success btn-sm">Success</button>
+      </div>
+      <div class="col">
+        <p>Danger</p>
+        <button class="btn btn-danger btn-sm">Danger</button>
+      </div>
+      <div class="col">
+        <p>Warning</p>
+        <button class="btn btn-warning btn-sm">Warning</button>
+      </div>
+      <div class="col">
+        <p>Info</p>
+        <button class="btn btn-info btn-sm">Info</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardArticle',
+  props: {
+    title: String
+  }
+}
+</script>
+
+<style scoped>
+p {
+  margin-bottom: 1px;
+}
+</style>
